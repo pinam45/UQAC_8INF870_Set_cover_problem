bf683535b905d2441f10b6bdc76ed32e17ea1df5
Maxime Pinard <maxime.pin@live.fr>
2019-03-12 11:03:13 -0400

    Updated main
    Check there exist a solution for the problem

--- solver/src/main.cpp
+++ solver/src/main.cpp
@@ -1,7 +1,44 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#include "logger.hpp"
+#include "Problem.hpp"
+
 #include <iostream>
+#include <dynamic_bitset.hpp>
+#include <vector>
+#include <list>
+
+namespace
+{
+	constexpr const char* PROBLEM_FILE_PATH = "resources/scp41.txt";
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	if(!init_logger())
+	{
+		return EXIT_FAILURE;
+	}
+
+	scp::Problem problem;
+	if(!scp::read_problem(PROBLEM_FILE_PATH, problem))
+	{
+		LOGGER->error("Failed to read problem");
+		return EXIT_FAILURE;
+	}
+	std::cout << problem;
+
+	dynamic_bitset<> cover_check(problem.full_set.size());
+	for(const auto& subset_points: problem.subsets_points)
+	{
+		cover_check |= subset_points;
+	}
+	assert(cover_check == problem.full_set);
+
+	return EXIT_SUCCESS;
 }
