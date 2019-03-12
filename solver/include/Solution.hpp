41093f8a8611fd3d09d8c513ab0fb8e08c2a166e
Maxime Pinard <maxime.pin@live.fr>
2019-03-12 14:43:20 -0400

    Added Solution

---
+++ solver/include/Solution.hpp
@@ -0,0 +1,43 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#ifndef SCPSOLVER_SOLUTION_HPP
+#define SCPSOLVER_SOLUTION_HPP
+
+#include "Problem.hpp"
+
+#include <dynamic_bitset.hpp>
+#include <spdlog/fmt/ostr.h> // for operator<<
+
+#include <filesystem>
+#include <ostream>
+
+namespace scp
+{
+	struct Solution final
+	{
+		static constexpr size_t invalid_cost = 0;
+
+		const Problem& problem;
+		dynamic_bitset<> selected_subsets;
+		dynamic_bitset<> covered_points;
+		bool cover_all_points;
+		size_t cost;
+
+		explicit Solution(const Problem& problem) noexcept;
+		Solution(const Solution&) = default;
+		Solution(Solution&&) noexcept = default;
+		Solution& operator=(const Solution&) = delete;
+		Solution& operator=(Solution&&) noexcept = delete;
+
+		void compute_cover() noexcept;
+		void compute_cost() noexcept;
+	};
+	std::ostream& operator<<(std::ostream& os, const Solution& solution);
+} // namespace scp
+
+#endif //SCPSOLVER_SOLUTION_HPP
---
+++ solver/src/Solution.cpp
@@ -0,0 +1,70 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#include "Solution.hpp"
+
+#include <ostream_config_guard.hpp>
+
+scp::Solution::Solution(const scp::Problem& problem_) noexcept
+  : problem(problem_)
+  , selected_subsets(problem_.subsets_points.size())
+  , covered_points(problem_.full_set.size())
+  , cover_all_points(false)
+  , cost(invalid_cost)
+{
+}
+
+void scp::Solution::compute_cover() noexcept
+{
+	assert(selected_subsets.size() == problem.subsets_points.size());
+	assert(covered_points.size() == problem.full_set.size());
+
+	covered_points.reset();
+	size_t selected_subset = selected_subsets.find_first();
+	while(selected_subset != dynamic_bitset<>::npos)
+	{
+		covered_points |= problem.subsets_points[selected_subset];
+		selected_subset = selected_subsets.find_next(selected_subset);
+	}
+	cover_all_points = covered_points.all();
+}
+
+void scp::Solution::compute_cost() noexcept
+{
+	assert(selected_subsets.size() == problem.subsets_points.size());
+
+	cost = 0;
+	size_t selected_subset = selected_subsets.find_first();
+	while(selected_subset != dynamic_bitset<>::npos)
+	{
+		cost += problem.subsets_costs[selected_subset];
+		selected_subset = selected_subsets.find_next(selected_subset);
+	}
+}
+
+std::ostream& scp::operator<<(std::ostream& os, const scp::Solution& solution)
+{
+	assert(solution.selected_subsets.size() == solution.problem.subsets_points.size());
+	assert(solution.covered_points.size() == solution.problem.full_set.size());
+
+	os << "Solution{\n";
+	os << "\tselected subsets number = " << solution.selected_subsets.count() << ",\n";
+	os << "\tselected subsets = ";
+	size_t selected_subset = solution.selected_subsets.find_first();
+	while(selected_subset != dynamic_bitset<>::npos)
+	{
+		os << selected_subset << ", ";
+		selected_subset = solution.selected_subsets.find_next(selected_subset);
+	}
+	os << "\n";
+	os << "\tcovered points = " << solution.covered_points << ",\n";
+	ostream_config_guard guard(os, std::boolalpha);
+	os << "\tcover all points = " << solution.cover_all_points << ",\n";
+	os << "\tcost = " << solution.cost << ",\n";
+	os << "}\n";
+	return os;
+}
