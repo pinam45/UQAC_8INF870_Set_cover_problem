100855febf5136c4ba8c168238baba5c35b7502f
Maxime Pinard <maxime.pin@live.fr>
2019-03-15 19:45:51 -0400

    Disabled warnings on spdlog headers

--- solver/cmake/spdlog.cmake
+++ solver/cmake/spdlog.cmake
@@ -8,6 +8,15 @@ if(is_empty)
 	message(FATAL_ERROR "Spdlog dependency is missing, maybe you didn't pull the git submodules")
 endif()
 
+# Include spdlog
 add_subdirectory(${SPDLOG_DIR})
+if(NOT TARGET spdlog)
+	message(FATAL_ERROR "spdlog target is missing")
+endif ()
+
+# Disable warnings on spdlog headers
+get_target_property(spdlog_include_directories spdlog INTERFACE_INCLUDE_DIRECTORIES)
+set_target_properties(spdlog PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
+target_include_directories(spdlog SYSTEM INTERFACE ${spdlog_include_directories})
 
 message(STATUS "Configuring spdlog - Done")
