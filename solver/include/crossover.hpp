47fff4737348ba982ca3643df3ba5f94f2b0566c
Benoît CORTIER <benoit.cortier@fried-world.eu>
2019-03-18 18:15:08 -0400

    Add random_merge and fat_merge crossovers

--- solver/include/crossover.hpp
+++ solver/include/crossover.hpp
@@ -5,6 +5,8 @@
 #ifndef SCPSOLVER_CROSSOVER_HPP
 #define SCPSOLVER_CROSSOVER_HPP
 
+#include <random>
+
 #include "Solution.hpp"
 
 namespace scp::crossover
@@ -12,6 +14,14 @@ namespace scp::crossover
 	/// A crossover operation that solve the subproblem obtained
 	/// by the union of the used subsets set of both solution.
 	Solution solve_subproblem_from(const Solution& sol1, const Solution& sol2);
+
+	// Keep randomly selected subsets from both parents.
+	Solution random_merge(const scp::Solution& sol1,
+	                      const scp::Solution& sol2,
+	                      std::default_random_engine& generator);
+
+	/// Keep all selected subsets from both parents.
+	Solution fat_merge(const Solution& sol1, const Solution& sol2);
 } // namespace scp::crossover
 
 #endif //SCPSOLVER_CROSSOVER_HPP
--- solver/src/crossover.cpp
+++ solver/src/crossover.cpp
@@ -2,6 +2,8 @@
 // Created by Benoit Cortier on 3/14/19.
 //
 
+#include <dynamic_bitset.hpp>
+
 #include "crossover.hpp"
 #include "greedy.hpp"
 
@@ -9,4 +11,43 @@ scp::Solution scp::crossover::solve_subproblem_from(const scp::Solution& sol1,
                                                     const scp::Solution& sol2)
 {
 	return greedy::weighted_solve(sol1.problem, sol1.selected_subsets | sol2.selected_subsets);
+}
+
+scp::Solution scp::crossover::random_merge(const scp::Solution& sol1,
+                                           const scp::Solution& sol2,
+                                           std::default_random_engine& generator)
+{
+	dynamic_bitset<> selectable_subsets = sol1.selected_subsets | sol2.selected_subsets;
+	size_t selectable_count = selectable_subsets.count();
+
+	scp::Solution sol(sol1.problem);
+
+	while(!sol.cover_all_points)
+	{
+		size_t selected_subset =
+		  std::uniform_int_distribution<size_t>(0, selectable_count - 1)(generator);
+		--selectable_count;
+
+		size_t idx = selectable_subsets.find_first();
+		for(size_t i = 0; i < selected_subset; ++i)
+		{
+			idx = selectable_subsets.find_next(idx);
+		}
+		sol.selected_subsets.set(idx);
+		selectable_subsets.reset(idx);
+
+		sol.compute_cover();
+	}
+
+	sol.compute_cost();
+	return sol;
+}
+
+scp::Solution scp::crossover::fat_merge(const scp::Solution& sol1, const scp::Solution& sol2)
+{
+	scp::Solution sol(sol1.problem);
+	sol.selected_subsets = sol1.selected_subsets | sol2.selected_subsets;
+	sol.compute_cover();
+	sol.compute_cost();
+	return sol;
 }
