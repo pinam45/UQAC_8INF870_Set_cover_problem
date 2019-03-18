daf9c644adc432978df173f7aa81b2a869ba11fc
Maxime Pinard <maxime.pin@live.fr>
2019-03-18 18:00:10 -0400

    Added PermutationsGenerator

--- solver/include/permutations.hpp
+++ solver/include/permutations.hpp
@@ -8,8 +8,34 @@
 #ifndef SCPSOLVER_PERMUTATIONS_HPP
 #define SCPSOLVER_PERMUTATIONS_HPP
 
+#include <memory>
+
 #include <dynamic_bitset.hpp>
 
 std::vector<std::vector<dynamic_bitset<>>> generate_permutations(size_t bitsets_size);
+
+class PermutationsGenerator
+{
+public:
+	PermutationsGenerator(size_t bitset_size, size_t ones_number) noexcept;
+
+	size_t bitset_size() const noexcept;
+	size_t ones_number() const noexcept;
+
+	void reset() noexcept;
+	void reset(size_t ones_number) noexcept;
+
+	const dynamic_bitset<>& next() noexcept;
+
+	bool finished() noexcept;
+
+private:
+	bool m_finished;
+	bool m_bit_to_add;
+	const size_t m_bitset_size;
+	size_t m_ones_number;
+	dynamic_bitset<> m_current;
+	std::unique_ptr<PermutationsGenerator> m_lower_permutation;
+};
 
 #endif //SCPSOLVER_PERMUTATIONS_HPP
--- solver/src/permutations.cpp
+++ solver/src/permutations.cpp
@@ -55,3 +55,85 @@ std::vector<std::vector<dynamic_bitset<>>> generate_permutations(size_t bitsets_
 
 	return permutations;
 }
+
+PermutationsGenerator::PermutationsGenerator(size_t bitset_size, size_t ones_number) noexcept
+  : m_finished(false)
+  , m_bit_to_add(false)
+  , m_bitset_size(bitset_size)
+  , m_ones_number(ones_number)
+  , m_current(bitset_size)
+  , m_lower_permutation((bitset_size > ones_number) && (ones_number > 0)
+                          ? std::make_unique<PermutationsGenerator>(bitset_size - 1, ones_number)
+                          : nullptr)
+{
+}
+
+size_t PermutationsGenerator::bitset_size() const noexcept
+{
+	return m_bitset_size;
+}
+
+size_t PermutationsGenerator::ones_number() const noexcept
+{
+	return m_ones_number;
+}
+
+void PermutationsGenerator::reset() noexcept
+{
+	m_finished = false;
+	m_bit_to_add = false;
+	m_current.reset();
+}
+
+void PermutationsGenerator::reset(size_t ones_number) noexcept
+{
+	assert(ones_number < m_bitset_size);
+	m_finished = false;
+	m_bit_to_add = false;
+	m_ones_number = ones_number;
+	m_current.reset();
+
+	if((m_bitset_size > m_ones_number) && (m_ones_number > 0))
+	{
+		m_lower_permutation =
+		  std::make_unique<PermutationsGenerator>(m_bitset_size - 1, m_ones_number);
+	}
+}
+
+const dynamic_bitset<>& PermutationsGenerator::next() noexcept
+{
+	if(m_ones_number == 0) // only 0
+	{
+		m_finished = true;
+	}
+	else if(m_bitset_size == m_ones_number) // only 1
+	{
+		m_current.set();
+		m_finished = true;
+	}
+	else // general case
+	{
+		assert(m_lower_permutation != nullptr);
+		m_current = m_lower_permutation->next();
+		m_current.push_back(m_bit_to_add);
+
+		if(m_lower_permutation->finished())
+		{
+			if(m_bit_to_add)
+			{
+				m_finished = true;
+			}
+			else
+			{
+				m_bit_to_add = true;
+				m_lower_permutation->reset(m_ones_number - 1);
+			}
+		}
+	}
+	return m_current;
+}
+
+bool PermutationsGenerator::finished() noexcept
+{
+	return m_finished;
+}
