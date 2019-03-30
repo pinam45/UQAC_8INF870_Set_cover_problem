b1284ba7ff59c38b5be41699902863dafe676253
Maxime Pinard <maxime.pin@live.fr>
2019-03-30 16:36:14 -0400

    Removed instance set e from nomad optimisation script

--- nomad/optimize.sh
+++ nomad/optimize.sh
@@ -10,8 +10,6 @@ run_problem "scpa1" $1
 run_problem "scpb1" $1
 run_problem "scpc1" $1
 run_problem "scpd1" $1
-run_problem "scpe1" $1
 run_problem "scp41" $1
 run_problem "scp51" $1
 run_problem "scp61" $1
-
